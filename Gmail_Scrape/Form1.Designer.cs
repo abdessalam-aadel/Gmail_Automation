6d69bfa05054821eb0bd2a74c4f635b748df7b9f
abdessalam-aadel <abdessalam.aadel@gmail.com>
2019-12-22 14:51:08 +0100

    First Commit

---
+++ Gmail_Scrape/Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace Gmail_Scrape
+{
+    partial class frm_Gmail
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Login = new System.Windows.Forms.Button();
+            this.btn_Delete = new System.Windows.Forms.Button();
+            this.btn_Exit = new System.Windows.Forms.Button();
+            this.txtBox_User = new System.Windows.Forms.TextBox();
+            this.txtBox_Pass = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.rtxtBox = new System.Windows.Forms.RichTextBox();
+            this.rtxtBox2 = new System.Windows.Forms.RichTextBox();
+            this.btnAbout = new System.Windows.Forms.Label();
+            this.btnDeletSpecific = new System.Windows.Forms.Button();
+            this.btnShow = new System.Windows.Forms.Label();
+            this.btnShow2 = new System.Windows.Forms.Label();
+            this.AboutHover1 = new System.Windows.Forms.Label();
+            this.labelHide = new System.Windows.Forms.Label();
+            this.labelShow = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_Login
+            // 
+            this.btn_Login.BackColor = System.Drawing.Color.LightGray;
+            this.btn_Login.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_Login.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Login.Location = new System.Drawing.Point(12, 118);
+            this.btn_Login.Name = "btn_Login";
+            this.btn_Login.Size = new System.Drawing.Size(231, 50);
+            this.btn_Login.TabIndex = 3;
+            this.btn_Login.Text = "Login";
+            this.btn_Login.UseVisualStyleBackColor = false;
+            this.btn_Login.Click += new System.EventHandler(this.btn_Login_Click);
+            // 
+            // btn_Delete
+            // 
+            this.btn_Delete.Enabled = false;
+            this.btn_Delete.Location = new System.Drawing.Point(12, 174);
+            this.btn_Delete.Name = "btn_Delete";
+            this.btn_Delete.Size = new System.Drawing.Size(231, 28);
+            this.btn_Delete.TabIndex = 4;
+            this.btn_Delete.Text = "Delete Message view";
+            this.btn_Delete.UseVisualStyleBackColor = true;
+            // 
+            // btn_Exit
+            // 
+            this.btn_Exit.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btn_Exit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_Exit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Exit.Location = new System.Drawing.Point(12, 240);
+            this.btn_Exit.Name = "btn_Exit";
+            this.btn_Exit.Size = new System.Drawing.Size(231, 23);
+            this.btn_Exit.TabIndex = 8;
+            this.btn_Exit.Text = "Exit";
+            this.btn_Exit.UseVisualStyleBackColor = false;
+            this.btn_Exit.Click += new System.EventHandler(this.btn_Exit_Click);
+            // 
+            // txtBox_User
+            // 
+            this.txtBox_User.Location = new System.Drawing.Point(16, 29);
+            this.txtBox_User.Name = "txtBox_User";
+            this.txtBox_User.Size = new System.Drawing.Size(227, 20);
+            this.txtBox_User.TabIndex = 1;
+            // 
+            // txtBox_Pass
+            // 
+            this.txtBox_Pass.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBox_Pass.Location = new System.Drawing.Point(15, 82);
+            this.txtBox_Pass.Name = "txtBox_Pass";
+            this.txtBox_Pass.PasswordChar = '▪';
+            this.txtBox_Pass.Size = new System.Drawing.Size(228, 20);
+            this.txtBox_Pass.TabIndex = 2;
+            this.txtBox_Pass.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtBox_Pass_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Email :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Password :";
+            // 
+            // rtxtBox
+            // 
+            this.rtxtBox.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.rtxtBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtxtBox.Location = new System.Drawing.Point(249, 29);
+            this.rtxtBox.Name = "rtxtBox";
+            this.rtxtBox.Size = new System.Drawing.Size(130, 98);
+            this.rtxtBox.TabIndex = 5;
+            this.rtxtBox.Text = "";
+            // 
+            // rtxtBox2
+            // 
+            this.rtxtBox2.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.rtxtBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtxtBox2.Location = new System.Drawing.Point(249, 133);
+            this.rtxtBox2.Name = "rtxtBox2";
+            this.rtxtBox2.Size = new System.Drawing.Size(130, 130);
+            this.rtxtBox2.TabIndex = 6;
+            this.rtxtBox2.Text = "";
+            // 
+            // btnAbout
+            // 
+            this.btnAbout.AutoSize = true;
+            this.btnAbout.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnAbout.ForeColor = System.Drawing.Color.DimGray;
+            this.btnAbout.Location = new System.Drawing.Point(344, 9);
+            this.btnAbout.Name = "btnAbout";
+            this.btnAbout.Size = new System.Drawing.Size(35, 13);
+            this.btnAbout.TabIndex = 7;
+            this.btnAbout.Text = "About";
+            this.btnAbout.Click += new System.EventHandler(this.btnAbout_Click);
+            this.btnAbout.MouseLeave += new System.EventHandler(this.btnAbout_MouseLeave);
+            // 
+            // btnDeletSpecific
+            // 
+            this.btnDeletSpecific.Enabled = false;
+            this.btnDeletSpecific.Location = new System.Drawing.Point(12, 206);
+            this.btnDeletSpecific.Name = "btnDeletSpecific";
+            this.btnDeletSpecific.Size = new System.Drawing.Size(231, 28);
+            this.btnDeletSpecific.TabIndex = 9;
+            this.btnDeletSpecific.Text = "Delete Messages with a specific title";
+            this.btnDeletSpecific.UseVisualStyleBackColor = true;
+            // 
+            // btnShow
+            // 
+            this.btnShow.AutoSize = true;
+            this.btnShow.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnShow.ForeColor = System.Drawing.Color.DimGray;
+            this.btnShow.Location = new System.Drawing.Point(227, 66);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(16, 13);
+            this.btnShow.TabIndex = 10;
+            this.btnShow.Text = "◉";
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // btnShow2
+            // 
+            this.btnShow2.AutoSize = true;
+            this.btnShow2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnShow2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnShow2.Location = new System.Drawing.Point(227, 61);
+            this.btnShow2.Name = "btnShow2";
+            this.btnShow2.Size = new System.Drawing.Size(19, 20);
+            this.btnShow2.TabIndex = 11;
+            this.btnShow2.Text = "●";
+            this.btnShow2.Click += new System.EventHandler(this.btnShow2_Click);
+            // 
+            // AboutHover1
+            // 
+            this.AboutHover1.AutoSize = true;
+            this.AboutHover1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.AboutHover1.ForeColor = System.Drawing.Color.OrangeRed;
+            this.AboutHover1.Location = new System.Drawing.Point(344, 9);
+            this.AboutHover1.Name = "AboutHover1";
+            this.AboutHover1.Size = new System.Drawing.Size(35, 13);
+            this.AboutHover1.TabIndex = 12;
+            this.AboutHover1.Text = "About";
+            this.AboutHover1.MouseHover += new System.EventHandler(this.AboutHover1_MouseHover);
+            // 
+            // labelHide
+            // 
+            this.labelHide.AutoSize = true;
+            this.labelHide.ForeColor = System.Drawing.Color.Gray;
+            this.labelHide.Location = new System.Drawing.Point(131, 66);
+            this.labelHide.Name = "labelHide";
+            this.labelHide.Size = new System.Drawing.Size(100, 13);
+            this.labelHide.TabIndex = 14;
+            this.labelHide.Text = "Hide your password";
+            // 
+            // labelShow
+            // 
+            this.labelShow.AutoSize = true;
+            this.labelShow.ForeColor = System.Drawing.Color.LightSeaGreen;
+            this.labelShow.Location = new System.Drawing.Point(126, 66);
+            this.labelShow.Name = "labelShow";
+            this.labelShow.Size = new System.Drawing.Size(105, 13);
+            this.labelShow.TabIndex = 15;
+            this.labelShow.Text = "Show your password";
+            // 
+            // frm_Gmail
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(387, 272);
+            this.Controls.Add(this.labelShow);
+            this.Controls.Add(this.labelHide);
+            this.Controls.Add(this.AboutHover1);
+            this.Controls.Add(this.btnShow2);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.btnDeletSpecific);
+            this.Controls.Add(this.btnAbout);
+            this.Controls.Add(this.rtxtBox2);
+            this.Controls.Add(this.rtxtBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBox_Pass);
+            this.Controls.Add(this.txtBox_User);
+            this.Controls.Add(this.btn_Exit);
+            this.Controls.Add(this.btn_Delete);
+            this.Controls.Add(this.btn_Login);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.KeyPreview = true;
+            this.Name = "frm_Gmail";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Gmail Automation";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Login;
+        private System.Windows.Forms.Button btn_Delete;
+        private System.Windows.Forms.Button btn_Exit;
+        private System.Windows.Forms.TextBox txtBox_User;
+        private System.Windows.Forms.TextBox txtBox_Pass;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox rtxtBox;
+        private System.Windows.Forms.RichTextBox rtxtBox2;
+        private System.Windows.Forms.Label btnAbout;
+        private System.Windows.Forms.Button btnDeletSpecific;
+        private System.Windows.Forms.Label btnShow;
+        private System.Windows.Forms.Label btnShow2;
+        private System.Windows.Forms.Label AboutHover1;
+        private System.Windows.Forms.Label labelHide;
+        private System.Windows.Forms.Label labelShow;
+    }
+}
+
---
+++ Gmail_Scrape/Form1.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System.Text.RegularExpressions;
+using Keys = System.Windows.Forms.Keys;
+
+namespace Gmail_Scrape
+{
+
+    public partial class frm_Gmail : Form
+    {
+        IWebDriver driver;
+
+        public frm_Gmail()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Login_Click(object sender, EventArgs e)
+        {
+            // Enable Form and Change cursor
+            this.Enabled = false;
+            this.Cursor = Cursors.WaitCursor;
+            using (driver = new ChromeDriver())
+            {
+                try
+                {
+                    // Satrt Explicit Waits
+                    WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(120));
+                    // Navigate to Gmail.com
+                    driver.Navigate().GoToUrl("https://mail.google.com/mail/");
+                    
+                    // email
+                    string email = txtBox_User.Text;
+                    // password
+                    string pass = txtBox_Pass.Text;
+
+                    // Maximize Navigator
+                    driver.Manage().Window.Maximize();
+
+                    // Enter Email and click next button
+                    IWebElement emailSend = wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//*[@type='email']")));
+                    emailSend.SendKeys(email);
+                    IWebElement emailNextClick = wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//*[@id='identifierNext']")));
+                    emailNextClick.Click();
+
+                    // Enter Password and click next button
+                    IWebElement passSend = wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//*[@type='password']")));
+                    passSend.SendKeys(pass);
+                    IWebElement passNextClick = driver.FindElement(By.XPath("//*[@id='passwordNext']"));
+                    passNextClick.Click();
+
+                    // Click to Social Media Messages
+                    IWebElement value_SMClick = wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//tr[@role='tablist']/td[2]")));
+                    value_SMClick.Click();
+
+                    // Node of Selectionner Tout
+                    string SelectAll = "//div[@role='button' and @style='user-select: none;']/div[1]/span";
+                    // Node of button delete
+                    //string node_Delet = "//div[@class='nH aqK']/div/div/div/div[2]/div[3]";
+                    string node_Delet = "//div[@act='10' and @role='button']/div";
+                    
+                    // see if messages SM exit or not
+                    IWebElement slide = wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//*[@class='ar5 J-J5-Ji']")));
+                    string vide = slide.Text;
+                    string rtextSM = "Your \"Social Networks\" tab is empty";
+                    string totaleSM = "your Totale social media messages is : ";
+                    SelectAndDelet_All(driver, vide, SelectAll, node_Delet, totaleSM, rtextSM);
+                    
+                    // Click to Promotions Messages
+                    IWebElement PromotionClick = wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//tr[@role='tablist']/td[3]")));
+                    PromotionClick.Click();
+                    // see if messages PROMO exit or not
+                    slide = wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//*[@class='ar5 J-J5-Ji']")));
+                    vide = slide.Text;
+                    string rtextPr = "Your \"Promotions\" tab is empty";
+                    string totalePr = "And your Totale Promotions messages is : ";
+                    SelectAndDelet_All(driver, vide, SelectAll, node_Delet,totalePr, rtextPr);
+
+                    Thread.Sleep(10000);
+                    // Close Browser & Driver
+                    driver.Close();
+                    driver.Quit();
+                    // Show Message << your social media messages was deleted >> ..
+                    MessageBox.Show("Social Media & Promotions Messages was successful deleted"
+                        + "\n\n" + "Gongralutation ..",
+                        "Messages Deleted", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+
+            // Default value
+            this.Enabled = true;
+            this.Cursor = Cursors.Default;
+        }
+
+        private void SelectAndDelet_All(IWebDriver driver, string vide, string SelectAll, string node_Delet, string totale, string rtext)
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(120));
+            bool b = Regex.IsMatch(totale, "social");
+
+            if (vide != " ")
+            {
+                // Declare number of Messages in page
+                // Old Xpath
+                //string node_NbrOfmessage = "//div[@aria-label='Afficher plus de messages']/span/span[1]/span[2]";
+                // New Xpath : Number of messages in page
+                string node_NbrOfmessage = "//*[@style='position: relative;']/div[2]/div[1]/div[2]/div[1]/div[1]/div[1]/" +
+                    "div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/span[1]/div[1]/span[1]/span[1]/span[2]";
+                IWebElement nbrValue = wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath(node_NbrOfmessage)));
+
+                // Declare number of pages messages
+                // Old Xpath
+                //string node_NbrOfPages = "//div[@aria-label='Afficher plus de messages']/span/span[2]";
+                // New Xpath : Number of pages messages
+                string node_NbrOfPages = "//*[@style='position: relative;']/div[2]/div[1]/div[2]/div[1]/div[1]/div[1]/" +
+                    "div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/span[1]/div[1]/span[1]/span[2]";
+                IWebElement nbrOfPage = wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath(node_NbrOfPages)));
+
+                // Start Conversion
+                // convert number of messages in page to integer
+                string nbr_Messages = nbrValue.Text;
+                int j = Convert.ToInt32(nbr_Messages);
+
+                // convert number of pages messages to integer
+                string nbr_Pages = nbrOfPage.Text;
+                // Replace space
+                nbr_Pages = Regex.Replace(nbr_Pages, @"\s", "");
+                int h = Convert.ToInt32(nbr_Pages);
+                // End Conversion.
+
+                // Show informations in Riche Text Box ..
+                if (b)
+                    rtxtBox.Text = totale + h;
+                else
+                    rtxtBox2.Text = totale + h;
+
+                while (j != h)
+                {
+                    // Select All Messages in page
+                    IWebElement selectAllClick = wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath(SelectAll)));
+                    selectAllClick.Click();
+                    // Delet Messages Selected
+                    IWebElement DeletClick = wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath(node_Delet)));
+                    DeletClick.Click();
+
+                    // Sleep for 15 second to delet messages
+                    TimeSpan waitforsecond = new TimeSpan(0, 0, 10);
+                    Thread.Sleep(waitforsecond);
+
+                    // Restart Value
+                    nbrValue = wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath(node_NbrOfmessage)));
+                    nbrOfPage = wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath(node_NbrOfPages)));
+                    nbr_Messages = nbrValue.Text;
+                    j = Convert.ToInt32(nbr_Messages);
+                    nbr_Pages = nbrOfPage.Text;
+                    nbr_Pages = Regex.Replace(nbr_Pages, @"\s", "");
+                    h = Convert.ToInt32(nbr_Pages);
+                }
+
+                if (j == h)
+                {
+                    // Select All Messages in page
+                    IWebElement selectAllClick = wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath(SelectAll)));
+                    selectAllClick.Click();
+                    // Delet Messages Selected
+                    IWebElement DeletClick = wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath(node_Delet)));
+                    DeletClick.Click();
+                }
+            }
+            else
+            {
+                // Show informations in Riche Text Box ..
+                if (b)
+                    rtxtBox.Text = rtext;
+                else
+                    rtxtBox2.Text = rtext;
+            }
+        }
+
+        private void btn_Exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void txtBox_Pass_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                btn_Login.PerformClick();
+            }
+        }
+
+        private void btnAbout_Click(object sender, EventArgs e)
+        {
+            AboutBox frmAbout = new AboutBox();
+            frmAbout.ShowDialog();       
+        }
+
+        private void btnShow2_Click(object sender, EventArgs e)
+        {
+            txtBox_Pass.PasswordChar = '\0';
+            btnShow2.Visible = false;
+            labelShow.Visible = false;
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            txtBox_Pass.PasswordChar = char.Parse("▪");
+            btnShow2.Visible = true;
+            labelShow.Visible = true;
+        }
+
+        private void AboutHover1_MouseHover(object sender, EventArgs e)
+        {
+            AboutHover1.Visible = false;
+        }
+
+        private void btnAbout_MouseLeave(object sender, EventArgs e)
+        {
+            AboutHover1.Visible = true;
+        }
+    }
+}
